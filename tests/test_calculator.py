d1b2fa713cf94de062b9d67c82120411a660f8c5
ViniciusLira1311 <52613474+ViniciusLira1311@users.noreply.github.com>
2024-04-18 10:25:42 -0300

    Atribuindo novos testes para soma e subtração

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,37 +3,61 @@ from calculator import Calculator
 
 class Test_calculator(unittest.TestCase):
     
-    def test_soma(self):
+    def test_soma_com_dois_numeros_positivos(self):
         c = Calculator()
         self.assertEqual(c.soma(2, 8), 10)
         
-    def test_soma_2(self):
+    def test_soma_com_um_numero_negativo(self):
         c = Calculator()
         self.assertEqual(c.soma(-3, 7), 4)
 
-    def teste_soma_3(self):
+    def teste_soma_com_dois_numeros_negativos(self):
         c = Calculator()
         self.assertEqual(c.soma(-3, -4), -7)
 
-    def teste_soma_4(self):
+    def teste_soma_com_um_zero(self):
         c =  Calculator()
         self.assertEqual(c.soma(0, 10), 10)
+    
+    def test_soma_com_apenas_um_numero(self):
+        c = Calculator()
+        self.assertEqual(c.soma(10), None)
+    
+    def teste_soma_com_numeros_decimais(self):
+        c = Calculator()
+        self.assertEqual(c.soma(3.4, 7.3), 10.7)
 
-    def teste_subtracao(self):
+    def teste_soma_com_valores_nao_sendo_numero(self):
+        c = Calculator()
+        self.assertEqual(c.soma("abc", 7), None)
+
+    def teste_subtracao_com_dois_numeros_positivos(self):
         c = Calculator()
         self.assertEqual(c.subtracao(2, 8), -6)
 
-    def teste_subtracao_2(self):
+    def teste_subtracao_com_um_numero_negativo(self):
         c = Calculator()
         self.assertEqual(c.subtracao(-3, 7), -10)
 
-    def teste_subtracao_3(self):
+    def teste_subtracao_com_dois_numeros_negativos(self):
         c = Calculator()
         self.assertEqual(c.subtracao(-3, -4), 1)
 
-    def teste_subtracao_4(self):
+    def teste_subtracao_com_um_zero(self):
         c = Calculator()
         self.assertEqual(c.subtracao(0, 10), -10)
+
+    def test_subtracao_com_apenas_um_numero(self):
+        c = Calculator()
+        self.assertEqual(c.subtracao(10), None)
+    
+    def teste_subtracao_com_numeros_decimais(self):
+        c = Calculator()
+        self.assertEqual(c.subtracao(3.4, 7.3), 10.7)
+
+    def teste_subtracao_com_valores_nao_sendo_numero(self):
+        c = Calculator()
+        self.assertEqual(c.subtracao("abc", 7), None)
 
 if __name__ == '__main__':
     unittest.main()
